3d072f4d7f798b380b21e20bea3cda0d523877d9
halilozel1903@gmail.com <halilozel1903@gmail.com>
2019-03-16 23:03:39 +0300

    gecis reklami eklendi.

--- app/src/main/java/com/halil/ozel/admobinterstitialads/MainActivity.java
+++ app/src/main/java/com/halil/ozel/admobinterstitialads/MainActivity.java
@@ -2,12 +2,38 @@ package com.halil.ozel.admobinterstitialads;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
+
+    private InterstitialAd mInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this, "ca-app-pub-4317187826403980~1326261366");
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+    }
+
+    public void showAds(View view) {
+
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAG", "The interstitial wasn't loaded yet.");
+        }
+
     }
 }
